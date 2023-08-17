861d5d80684b21036a27f96567af19271b4e8920
aldoud1799 <abdallah.abbas.jr@gmail.com>
2023-08-17 01:35:09 +0000

    4th

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,26 @@
 #!/usr/bin/env python3
-"""implement a get_page function (prototype: def get_page(url: str) -> str:).
-The core of the function is very simple. It uses the requests module
-to obtain the HTML content of a particular URL and returns it."""
-import redis
-import requests
+''' A module thst does stuff '''
 
-# Redis connection
-r = redis.Redis()
+import requests
+import redis
 
 
 def get_page(url: str) -> str:
-    """ Check if the URL content is already cached"""
-    cached_content = r.get(f"cached:{url}")
-    if cached_content:
-        # URL content found in cache, return it
-        return cached_content.decode('utf-8')
+    ''' return a urls content and tracks how many times it has been called'''
+    r  = redis.Redis()
 
-    # If URL content is not in cache, fetch it
+    count_key = f"count:{url}"
+    cache_key = f"cache:{url}"
+
+    r.incr(count_key)
+    r.expire(count_key, 10)
+
+    cached = r.get(cache_key)
+    if cached:
+        return cached.decode('utf-8')
+
     response = requests.get(url)
-
-    # Cache the content with an expiration time of 10 seconds
-    r.setex(f"cached:{url}", 10, response.text)
-
-    # Increment the count for the URL access
-    r.incr(f"count:{url}")
-
-    return response.text
-
-
-if __name__ == "__main__":
-    url = 'http://google.com'
-    # Get the initial count before calling get_page
-    initial_count = int(r.get(f"count:{url}") or 0)
-    print(f"Initial Count: {initial_count}")
-
-    # Call get_page
-    page_content = get_page(url)
-
-    # Get the count after calling get_page
-    current_count = int(r.get(f"count:{url}") or 0)
-    print(f"Current Count: {current_count}")
-    print(page_content)
+    content = response.text
+    r.setex(cache_key, 10, content)
+    
+    return content
